892ad13866741d8ae7f6f3b20b169d7875599bc0
taufikfy <mohtaufikfb923@gmail.com>
2025-02-25 01:44:40 +0700

    Update project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div id="copyright">
+            <div className="wrapper">
+                &copy; 2025. <b>RuangBelajar.</b> All Rights Reserved.
+            </div>
+        </div>
+        
+  )
+}
+
+export default Footer
